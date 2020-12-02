0c04af15d98356f00759236aab3c1a3efc960881
v4g4x <varun.gawande@gmail.com>
2020-12-02 13:30:19 +0530

    Finished Codes upto 11, and Mock too

---
+++ Mock/SHA/src/SHA1/SHA1.java
@@ -0,0 +1,36 @@
+package SHA1;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+//Write a program in C++ or java to implement
+//  SHA1 algorithm using libraries (API)
+
+public class SHA1 {
+    public static String hash(String input){
+        try{
+            // Get SHA1 message digest from library
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            //Convert string to byte array
+            byte[] digestedMessage = md.digest(input.getBytes());
+            //Convert byte[] to SigNum
+            BigInteger bi = new BigInteger(1,digestedMessage);
+            //Convert to Hex-Value
+            String hash = bi.toString(16);
+            //Pad with 0s on left until 32bit
+            while(hash.length() < 32) hash = "0"+hash;
+            return hash;
+        }
+        catch (NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string to input: ");
+        String str = sc.nextLine();
+        System.out.println("Hash of "+str+" is: "+hash(str));
+    }
+}
